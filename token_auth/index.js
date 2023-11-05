a3b48ca706e00690d2b60bb21050a1b5de01f89b
Ihor Klimov <iklimov6@gmail.com>
2023-11-05 16:13:06 -0500

    Add auth0 account verification

--- token_auth/index.js
+++ token_auth/index.js
@@ -6,10 +6,15 @@ const path = require('path');
 const port = 3000;
 const fs = require('fs');
 const jwt = require('jsonwebtoken');
+var request = require("request");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const clientSecret = "DGZzYm0Q6FjKX7SSgg259oaktZ69xfMcH_b2_s1nFbqjz4l8S0IJUgoK4fsly0sB";
+const clientId = "XKJpftAxA1wf4rd41rTlcCYlfKj0I3uS";
+const audience = 'https://dev-6kko8irjnrz18yn5.us.auth0.com/api/v2/';
 
 const SESSION_KEY = 'Authorization';
 
@@ -85,6 +90,53 @@ app.use((req, res, next) => {
     next();
 });
 
+app.get('/getToken', (req, res) => {
+    var options = {
+        method: 'POST',
+        url: 'https://dev-6kko8irjnrz18yn5.us.auth0.com/oauth/token',
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        form:
+        {
+            grant_type: 'password',
+            username: 'iklimov2@gmail.com',
+            password: 'M23n2gm23t(m1',
+            audience: audience,
+            scope: 'offline_access',
+            client_id: clientId,
+            client_secret: clientSecret
+        }
+    };
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        console.log(body);
+        res.send(body);
+    });
+});
+
+app.get('/refreshToken', (req, res) => {
+    var options = {
+        method: 'POST',
+        url: 'https://dev-6kko8irjnrz18yn5.us.auth0.com/oauth/token',
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        form:
+        {
+            grant_type: 'refresh_token',
+            client_id: clientId,
+            client_secret: clientSecret,
+            refresh_token: '9FOCx17w2H-Pxxtien-G4N13vyHuP_dPSzte21EjqQAq1'
+        }
+    };
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        console.log(body);
+        res.send(body)
+    });
+});
+
 app.get('/', (req, res) => {
     if (req.session.username) {
         return res.json({
@@ -107,7 +159,7 @@ const users = [
         username: 'Username',
     },
     {
-        login: 'Login1',
+        login: 'iklimov2@gmail.com',
         password: 'Password1',
         username: 'Username1',
     }
@@ -116,21 +168,49 @@ const users = [
 app.post('/api/login', (req, res) => {
     const { login, password } = req.body;
 
-    const user = users.find((user) => {
-        if (user.login == login && user.password == password) {
-            return true;
+    var options = {
+        method: 'POST',
+        url: 'https://dev-6kko8irjnrz18yn5.us.auth0.com/oauth/token',
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        form:
+        {
+            grant_type: 'password',
+            username: login,
+            password: password,
+            audience: audience,
+            scope: 'offline_access',
+            client_id: clientId,
+            client_secret: clientSecret
         }
-        return false
+    };
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        body = JSON.parse(body);
+        token = body.access_token;
+        console.log(token);
+        if (body.error){
+            res.status(401).send();
+            return false;
+        }
+
+        const user = users.find((user) => {
+            if (user.login == login) {
+                return true;
+            }
+            return false
+        });
+
+        if (user) {
+            req.session.username = user.username;
+            req.session.login = user.login;
+
+            res.json({ token: req.sessionId });
+        }
+
+        res.status(401).send();
     });
-
-    if (user) {
-        req.session.username = user.username;
-        req.session.login = user.login;
-
-        res.json({ token: req.sessionId });
-    }
-
-    res.status(401).send();
 });
 
 app.listen(port, () => {
